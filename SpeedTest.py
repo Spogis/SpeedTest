33e3256e6e10a728f99668f6d71c0ecf437789c2
Nicolas Spogis <nicolas.spogis@gmail.com>
2021-03-04 15:00:28 -0300

    Initial commit

---
+++ SpeedTest.py
@@ -0,0 +1,42 @@
+# Python program to test 
+# internet speed 
+  
+import speedtest  
+import time
+from openpyxl import load_workbook
+from datetime import datetime
+
+speedtester = speedtest.Speedtest()
+
+SleepTime = 10 #Minutes 
+TotalTime = 120 #Minutes
+
+NumberOfData = int(TotalTime/SleepTime)
+
+# Start by opening the spreadsheet and selecting the main sheet
+workbook = load_workbook(filename="SpeedTestData.xlsx")
+sheet = workbook.active
+
+
+def get_final_speed():
+    speedtester.get_best_server()
+    SpeedDownload = speedtester.download() / 1e+6
+    SpeedDownload = round(SpeedDownload,2)
+    
+    SpeedUpload = speedtester.upload() / 1e+6
+    SpeedUpload = round(SpeedUpload, 2)
+    
+    SpeedPing = round(speedtester.results.ping, 1)
+    return [SpeedPing, SpeedDownload, SpeedUpload];
+
+ 
+for i in range(NumberOfData):
+    x=get_final_speed()
+    rows = sheet.max_row
+    print("Time %s SpeedTest %s" % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), x))
+    sheet.cell(row=rows+1, column=1).value = datetime.now()
+    sheet.cell(row=rows+1, column=2).value =x[0]
+    sheet.cell(row=rows+1, column=3).value =x[1]
+    sheet.cell(row=rows+1, column=4).value =x[2]
+    workbook.save(filename="SpeedTestData.xlsx")
+    time.sleep(SleepTime*60)
